eecdb376da3b41eac315ee1e029d9bad60d2b93a
Theo-Grillon <theo.grillon@laposte.net>
2022-02-10 13:33:05 -0500

    YoumuController.cs (bugged)

--- Assets/Scripts/Control/YoumuController.cs
+++ Assets/Scripts/Control/YoumuController.cs
@@ -24,6 +24,7 @@ namespace Control
         {
             get;
             set;
+            ///Don't know where to put these lines
             ///Debug.Log("Hit!");
             ///Health.instance.loseHealth(1);
         }
