82be263fe5bf9fc168af74ef2d5d01f41538154d
Theo-Grillon <theo.grillon@laposte.net>
2022-02-28 01:03:40 -0500

    passive health regen card, no test done because of unity bugs

--- Assets/Scripts/CardSystem/PassiveCard.cs
+++ Assets/Scripts/CardSystem/PassiveCard.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using Control;
+using Control.YoumuController;
+using LogicSystem;
+using LogicSystem.Health;
 
 namespace CardSystem
 {
     public abstract class PassiveCard: Card
     {
-
+        public static YoumuController instance{get; set;}
+        protected Health healthBar;
+        protected int frames;
     }
 }
