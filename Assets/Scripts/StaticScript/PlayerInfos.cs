c0954ba001b0de80b00cfef5930bb07355e90cfb
Begin Florian <misterluckyyt@gmail.com>
2022-03-14 17:57:42 -0400

    rename fights

--- Assets/Scripts/StaticScript/PlayerInfos.cs
+++ Assets/Scripts/StaticScript/PlayerInfos.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CardSystem;
 
-public class PlayerInfos : MonoBehaviour
+public static class PlayerInfos
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static ActiveCard[] playerUnlockedActiveCard { get; set; }
 }
