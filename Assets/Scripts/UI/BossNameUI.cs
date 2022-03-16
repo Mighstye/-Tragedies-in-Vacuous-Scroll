84340a2e6079657469408d02cc43b1a22dcf3b47
Myonmu <62897460+Myonmu@users.noreply.github.com>
2022-03-16 19:42:21 -0400

    added boss name localization

---
+++ Assets/Scripts/UI/BossNameUI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace UI
+{
+    public class BossNameUI : MonoBehaviour
+    {
+        [SerializeField] private LocalizedString localizedBossName;
+        private TextMeshProUGUI nameLabel;
+        public string currentBossName = ""; //Test field
+
+        private void Start()
+        {
+            localizedBossName.TableReference = "Boss Names";
+            nameLabel = GetComponent<TextMeshProUGUI>();
+            UpdateBossName(currentBossName); //Test
+        }
+
+        public void UpdateBossName(string bossName)
+        {
+            localizedBossName.TableEntryReference = bossName;
+            StartCoroutine(Localize());
+        }
+        
+        private IEnumerator Localize()
+        {
+            var localizedString = localizedBossName.GetLocalizedStringAsync();
+            yield return localizedString;
+            if(localizedString.IsDone)nameLabel.text=localizedString.Result;
+        }
+    }
+}
