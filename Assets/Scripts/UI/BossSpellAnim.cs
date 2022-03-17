0f320b6e5f849881a0c93eeb03e894428a107a5d
Myonmu <62897460+Myonmu@users.noreply.github.com>
2022-03-16 21:01:30 -0400

    added boss spell ui anim

---
+++ Assets/Scripts/UI/BossSpellAnim.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Logic_System;
+using UnityEngine;
+
+namespace UI
+{
+    public class BossSpellAnim : MonoBehaviour
+    {
+        [SerializeField]private Sprite bossSprite;
+        
+        private Animation anim;
+
+        private BattleOutcome battleOutcomeRef;
+        private void Start()
+        {
+            anim = GetComponent<Animation>();
+            battleOutcomeRef = LogicSystemAPI.instance.battleOutcome;
+            battleOutcomeRef.onPhaseStart += (s, statistics) =>
+            {
+                if (s is null) return;
+                anim.Play();
+            };
+        }
+
+        public void Init(Sprite sprite)
+        {
+            bossSprite = sprite;
+        }
+        
+        
+        
+    }
+}
