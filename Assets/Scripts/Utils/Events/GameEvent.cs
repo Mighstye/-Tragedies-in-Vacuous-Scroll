c1988caab5102e2e327f14688a59cf62ffdb6b81
Myonmu <62897460+Myonmu@users.noreply.github.com>
2022-03-26 15:37:22 -0400

    rename folders

---
+++ Assets/Scripts/Utils/Events/GameEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    [CreateAssetMenu(fileName = "Event", menuName = "Events/Base Game Event", order = 0)]
+    public class GameEvent : ScriptableObject
+    {
+        private readonly List<GameEventSubscriber> subscribers = 
+            new List<GameEventSubscriber>();
+
+        public void FireEvent()
+        {
+            foreach (var t in subscribers)
+            {
+                t.OnEventFired();
+            }
+        }
+
+        public static GameEvent operator+(GameEvent evt, GameEventSubscriber sub)
+        {
+            evt.subscribers.Add(sub);
+            return evt;
+        }
+
+        public static GameEvent operator-(GameEvent evt, GameEventSubscriber sub)
+        {
+            evt.subscribers.Remove(sub);
+            return evt;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Events/GameEventSubscriber.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Utils
+{
+    public class GameEventSubscriber : MonoBehaviour
+    {
+        public GameEvent gameEvent;
+        public UnityEvent unityEvent;
+
+        public void OnEventFired()
+        {
+            unityEvent?.Invoke();
+        }
+
+        private void OnEnable()
+        {
+            gameEvent += this;
+        }
+
+        private void OnDisable()
+        {
+            gameEvent -= this;
+        }
+    }
+}
