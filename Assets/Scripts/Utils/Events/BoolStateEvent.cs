56170363cf0a94cdd47fe90ff3092af3995c9572
Myonmu <62897460+Myonmu@users.noreply.github.com>
2022-03-31 23:10:22 -0400

    update SO creation menu

--- Assets/Scripts/Utils/Events/BoolStateEvent.cs
+++ Assets/Scripts/Utils/Events/BoolStateEvent.cs
@@ -1,5 +1,7 @@
-﻿namespace Utils.Events
+﻿using UnityEngine;
+namespace Utils.Events
 {
+    [CreateAssetMenu(fileName = "BoolState", menuName = "Events/Bool State Event (Bool->void)", order = 0)]
     public class BoolStateEvent: GenericEvent<bool>
     {
         
