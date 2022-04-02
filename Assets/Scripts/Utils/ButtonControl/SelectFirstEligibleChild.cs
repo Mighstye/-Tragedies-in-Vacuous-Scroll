836fde785c275f80a6de9f2b3c8526f0ae2d206a
Myonmu <62897460+Myonmu@users.noreply.github.com>
2022-04-01 20:17:58 -0400

    added pause menu button control

---
+++ Assets/Scripts/Utils/ButtonControl/SelectFirstEligibleChild.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+namespace Utils
+{
+    public class SelectFirstEligibleChild : MonoBehaviour
+    {
+
+        private void OnEnable()
+        {
+            StartCoroutine(Select());
+        }
+        private IEnumerator Select()
+        {
+            var buttons = GetComponentsInChildren<Button>();
+            EventSystem.current.SetSelectedGameObject(null);
+            yield return new WaitForEndOfFrame();
+            foreach (var button in buttons) {
+                if (button.navigation.mode is Navigation.Mode.None) continue;
+                EventSystem.current.SetSelectedGameObject(gameObject);
+                yield break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/ButtonControl/SelectThisOnEnable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+namespace Utils
+{
+    [RequireComponent(typeof(Button))]
+    public class SelectThisOnEnable : MonoBehaviour
+    {
+        private Button button;
+        private void OnEnable()
+        {
+            button = GetComponent<Button>();
+            if (button.navigation.mode is Navigation.Mode.None) return;
+            StartCoroutine(Select());
+        }
+        private IEnumerator Select()
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            yield return new WaitForEndOfFrame();
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+        
+        
+    }
+}
