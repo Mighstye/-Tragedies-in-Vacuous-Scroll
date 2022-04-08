a6d02bf4eb6ea3c4a9b0fa674982006a05b695ec
Letho Norgil <theo.grillon@laposte.net>
2022-04-07 20:26:39 -0400

    Wrong files were modified

--- Assets/Scripts/BossBehaviourImplementations/YaotomeBehaviour/phase1.cs
+++ Assets/Scripts/BossBehaviourImplementations/YaotomeBehaviour/phase1.cs
@@ -9,7 +9,7 @@ public class phase1 : BossPhase
     protected override void OnPhaseStartCustom(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Debug.Log("Phase 1 started");
-        phase1.PhaseType = PhaseType.NonSpellPhase;
+        
         BulletLauncher1 Launcher = new BulletLauncher1();
         
     }
